815b7adba8714d9666cb2c905507774c8f8770a1
Morad <2465064e@student.gla.ac.uk>
2021-10-27 14:54:58 +0100

    Added - Views, URLs and starter templates for the majority of the pages which the site will need.

--- repairelapp/views.py
+++ repairelapp/views.py
@@ -10,3 +10,25 @@ class AboutView(TemplateView):
 
 class FAQView(TemplateView):
     template_name = 'faq.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+class RegistrationView(TemplateView):
+    template_name = 'registration.html'
+
+class ShoppingCartView(TemplateView):
+    template_name = 'shopping-cart.html'
+
+class EngageView(TemplateView):
+    template_name = 'engage.html'
+
+class SustainabilityView(TemplateView):
+    template_name = 'sustainability.html'
+
+class TermsView(TemplateView):
+    template_name = 'terms.html'
+
+
+
+
