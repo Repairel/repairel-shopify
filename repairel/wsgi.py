58d2b5d4be54cd53d13b890f65b4aa7e5ddd00c6
Roman Polek <polekroman@gmail.com>
2022-01-26 12:43:35 +0000

    updated wsgi

--- repairel/wsgi.py
+++ repairel/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from lecigon.settings import DEBUG
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repairel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lecigon.settings')
+
+if DEBUG:
+    print("---STARTING DEVELOPMENT SERVER---")
+else:
+    print("---STARTING PRODUCTION SERVER---")
+application = get_wsgi_application()
 
 application = get_wsgi_application()
