30faa7e562bc63fb171fe543b290b0be5cfefbe1
Roman Polek <polekroman@gmail.com>
2022-01-26 13:18:11 +0000

    fix

--- repairelapp/shopify_products.py
+++ repairelapp/shopify_products.py
@@ -1,17 +1,17 @@
 import requests
 try:
     from repairelapp import keys
+    url = 'https://%s:%s@repairel-dev.myshopify.com/admin/api/2021-10/' % (keys.API_KEY, keys.PASSWORD)
 except:
     #TODO pull keys from environment variables
-    keys = {
-        "API_KEY": '',
-        "API_SECRET": '',
-    }
+    url = None
     print("keys not found")
 
-url = 'https://%s:%s@repairel-dev.myshopify.com/admin/api/2021-10/' % (keys.API_KEY, keys.PASSWORD)
 
 def get_products():
-    endpoint = 'products.json'
-    r = requests.get(url + endpoint)
-    return r.json()
+    if(url):
+        endpoint = 'products.json'
+        r = requests.get(url + endpoint)
+        return r.json()
+    else:
+        return ""
