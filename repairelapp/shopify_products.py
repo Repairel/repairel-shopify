62016e2ed74bf280438b6b5ab0050160f935814d
Roman Polek <polekroman@gmail.com>
2022-01-26 13:15:23 +0000

    keys

--- repairelapp/shopify_products.py
+++ repairelapp/shopify_products.py
@@ -3,6 +3,10 @@ try:
     from repairelapp import keys
 except:
     #TODO pull keys from environment variables
+    keys = {
+        "API_KEY": '',
+        "API_SECRET": '',
+    }
     print("keys not found")
 
 url = 'https://%s:%s@repairel-dev.myshopify.com/admin/api/2021-10/' % (keys.API_KEY, keys.PASSWORD)
