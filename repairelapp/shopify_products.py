74f3bfb2ac4ac094ca973a05054770a27006f7de
Roman Polek <polekroman@gmail.com>
2022-02-09 21:17:56 +0000

    easy fix
    Co-authored-by: Morad <MoradEnCours@users.noreply.github.com>

--- repairelapp/shopify_products.py
+++ repairelapp/shopify_products.py
@@ -4,6 +4,9 @@ from repairelapp import keys
 url = 'https://%s:%s@repairel-dev.myshopify.com/admin/api/2021-10/' % (keys.API_KEY, keys.PASSWORD)
 
 def get_products():
-    endpoint = 'products.json'
+    #TODO to access metafields use the url: 'products/7461853069543/metafields.json'
+    #so 'products/<id>/metafields.json'
+    endpoint = "products.json"
     r = requests.get(url + endpoint)
+    print(r.json())
     return r.json()
